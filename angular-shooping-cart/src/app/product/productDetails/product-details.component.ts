6584a339adfa9237b74380916f1851a9ea92ca10
tahany fawzy <tahany.fawzy@asset.com.eg>
2018-02-14 10:46:27 +0200

    create product details and fix the images display

--- angular-shooping-cart/src/app/product/productDetails/product-details.component.ts
+++ angular-shooping-cart/src/app/product/productDetails/product-details.component.ts
@@ -1,10 +1,30 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Product } from "../product";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ProductService } from "../product.service";
 
 @Component({
-    selector : 'product-details',
-    templateUrl : 'product-details.component.html'
+    selector: 'product-details',
+    templateUrl: 'product-details.component.html'
 })
-export class ProductDetailsComponent{
-    
+export class ProductDetailsComponent implements OnInit {
+
+    product: Product;
+
+    constructor(private routeObj: ActivatedRoute,
+        private productService: ProductService,
+        private router: Router) {}
+
+    ngOnInit(): void {
+        let id = this.routeObj.snapshot.params['id'];
+        //snapshot because we will initialise every time we visit the page , 
+        //use observable if the id will change in the same page
+        this.productService.getProductById(id)
+            .subscribe(data => this.product = data);
+
+    }
+
+    onBack() {
+        this.router.navigate(['/products']);
+    }
 }
