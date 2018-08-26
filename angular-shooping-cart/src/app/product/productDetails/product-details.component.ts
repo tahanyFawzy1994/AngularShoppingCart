3dcdfd83c900cee579fef56078bdd88743644674
tahany fawzy <tahanyfawzy1994@gmail.com>
2018-08-26 10:43:14 +0200

    Update product-details.component.ts

--- angular-shooping-cart/src/app/product/productDetails/product-details.component.ts
+++ angular-shooping-cart/src/app/product/productDetails/product-details.component.ts
@@ -10,7 +10,7 @@ import { Location } from '@angular/common';
 })
 export class ProductDetailsComponent implements OnInit {
 
-    private product: Product;
+    public product: Product;
 
     constructor(private routeObj: ActivatedRoute,
         private productService: ProductService,
